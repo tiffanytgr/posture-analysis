2f804490dd83eda3d4b4ca8ab3b11fe54f4fb9e7
tiffanytgr <tiffanytan.gr@gmail.com>
2023-12-02 18:43:46 +0800

    update

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,13 +16,15 @@ import pickle
 import sounddevice as sd
 import pandas as pd
 import warnings
+import time
+
+import sklearn.metrics
 warnings.filterwarnings("ignore")
 
 
 
-
 # default demo video 
-DEMO_VIDEO = 'data/good_pose/sample-running-good-1.mp4'
+DEMO_VIDEO = 'good-video-9.mp4'
 
 
 #mediapipe inbuilt solutions 
@@ -50,17 +52,19 @@ def calculate_angle(a,b,c):
 def main():
 
     #title 
-    st.title('Pose Classification App')
+    st.title('Posture Classification App')
 
     #sidebar title
-    st.sidebar.title('Pose Classification App')
+    st.sidebar.title('Posture Classification App')
 
     st.sidebar.subheader('Parameters')
     #creating a button for webcam
     use_webcam = st.sidebar.button('Use Webcam')
     #creating a slider for detection confidence 
-    detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
+    # detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
     
+    url = 'https://github.com/tiffanytgr/posture-analysis-app/tree/main'
+    st.sidebar.markdown(f'''<a href={url}><button style="background-color:Grey;">Link to Github Code</button></a>''',unsafe_allow_html=True)
     #model selection 
     # model_selection = st.sidebar.selectbox('Model Selection',options=[0,1,2])
     # st.markdown(' ## Output')
@@ -99,8 +103,8 @@ def main():
     pose = mp_pose.Pose()
     class_name='Good'
 
-    st.sidebar.text('Input Video')
-    st.sidebar.video(tfflie.name)
+    # st.sidebar.text('Input Video')
+    # st.sidebar.video(tfflie.name)
 
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
@@ -116,161 +120,194 @@ def main():
         clf = pickle.load(f)
         count = 0
   
-    with mp_holistic.Holistic(min_detection_confidence=detection_confidence) as holistic:   
-        while vid.isOpened():
+    with mp_holistic.Holistic(min_detection_confidence=0.5) as holistic:   
+        
+        end_time = time.time()+60
+        while time.time() < end_time:
+            vid = cv2.VideoCapture(tfflie.name)
 
-            ret, frame = vid.read()
+            width = 640
+            height = 480
+            fps = int(vid.get(cv2.CAP_PROP_FPS))
+            # codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
+            codec = cv2.VideoWriter_fourcc('V','P','0','9')
+            out = cv2.VideoWriter('output1.webm', codec, fps, (width, height))
+            while vid.isOpened():
 
-            if ret == False:
-                break
-             #recoloring it back to BGR b/c it will rerender back to opencv
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
+                ret, frame = vid.read()
 
-            results = holistic.process(image)
-            # Process the image.
-            # Get height and width.
-            h, w = image.shape[:2]
+                if ret == False:
+                    break
+                #recoloring it back to BGR b/c it will rerender back to opencv
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
 
-            # Use lm and lmPose as representative of the following methods.
-            lm = results.pose_landmarks
-            lmPose = mp_pose.PoseLandmark
+                results = holistic.process(image)
+                # Process the image.
+                # Get height and width.
+                h, w = image.shape[:2]
+                font_size=int(w/25)-8
+                if w<400 and h>400:
+                    font_size=int(w/15)-5
+                # print(h)
+                # print(w)
 
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+                # Use lm and lmPose as representative of the following methods.
+                lm = results.pose_landmarks
+                lmPose = mp_pose.PoseLandmark
 
-            # Acquire the landmark coordinates.
-            # Once aligned properly, left or right should not be a concern.
-            try:
-                # Left shoulder.
-                l_shldr_x = int(lm.landmark[lmPose.LEFT_SHOULDER].x * w)
-                l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
-
-                # Right shoulder
-                r_shldr_x = int(lm.landmark[lmPose.RIGHT_SHOULDER].x * w)
-                r_shldr_y = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
-
-                # Left ear.
-                l_ear_x = int(lm.landmark[lmPose.LEFT_EAR].x * w)
-                l_ear_y = int(lm.landmark[lmPose.LEFT_EAR].y * h)
-                # Left hip.
-
-                l_hip_x = int(lm.landmark[lmPose.LEFT_HIP].x * w)
-                l_hip_y = int(lm.landmark[lmPose.LEFT_HIP].y * h)
-
-                # Calculate angle.
-                def findAngle(x1, y1, x2, y2):
-                    theta = m.acos((y2 -y1)*(-y1) / (m.sqrt((x2 - x1)**2 + (y2 - y1)**2) * y1))
-                    degree = int(180/m.pi)*theta
-                    return degree
-            
-                
-                # setting image writeable back to true to be able process it
-                image.flags.writeable = True
-                pil_im = Image.fromarray(image)
-                draw = ImageDraw.Draw(pil_im)
-
-                # def angle
-                torso_angle = findAngle(l_shldr_x, l_shldr_y, l_hip_x, l_hip_y)
-                torso_angle = "Shoulder-Hip Angle:" + str(round(torso_angle,2))
-                neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
-                neck_inclination = "Neck Inclination (Ear-Shoulder Angle):" + str(round(neck_inclination,2))
-                
-                # font
-                font = ImageFont.truetype(font = "C:/Windows/Fonts/Arial.ttf", size = 30)
-                
-                # draw text
-                draw.text((20,0), torso_angle, font = font)
-                image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-                draw.text((20,40), neck_inclination, font = font)
-                image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-
-                # Draw the pose landmarks
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
-                
-                len(results.pose_landmarks.landmark)
-                num_coords = len(results.pose_landmarks.landmark)
-                landmarks = ['class']
-                for val in range(1, num_coords+1):
-                    landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
-                with open('Prediction_info.csv', mode='w', newline='') as f:
-                    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow(landmarks)
-                class_name = 'abc'
-            except:
-                pass
-            try:
-                
-                pose = results.pose_landmarks.landmark
-                pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+                                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
 
-                row = pose_row 
-                row.insert(0, class_name)
-                
-                with open('Prediction_info.csv', mode='a', newline='') as f:
-                    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow(row)
+                # Acquire the landmark coordinates.
+                # Once aligned properly, left or right should not be a concern.
+                try:
+                    # Left shoulder.
+                    l_shldr_x = int(lm.landmark[lmPose.LEFT_SHOULDER].x * w)
+                    l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * h)
 
-                df = pd.read_csv('Prediction_info.csv')
-                nose = [df.at[0,'x1'],df.at[0,'y1']]
-                
-                left_eye = [df.at[0,'x3'],df.at[0,'y3']]
-                left_ear = [df.at[0, 'x8'], df.at[0, 'y8']]
-                left_shoulder = [df.at[0, 'x12'], df.at[0, 'y12']]
-                left_hip = [df.at[0, 'x24'], df.at[0, 'y24']]
-                left_knee = [df.at[0, 'x26'], df.at[0, 'y26']]
+                    # Right shoulder
+                    r_shldr_x = int(lm.landmark[lmPose.RIGHT_SHOULDER].x * w)
+                    r_shldr_y = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * h)
 
-                right_eye = [df.at[0,'x6'],df.at[0,'y6']]
-                right_ear = [df.at[0, 'x9'], df.at[0, 'y9']]
-                right_shoulder = [df.at[0, 'x13'], df.at[0, 'y13']]
-                right_hip = [df.at[0, 'x25'], df.at[0, 'y25']]
-                right_knee = [df.at[0, 'x27'], df.at[0, 'y27']]
+                    # Left ear.
+                    l_ear_x = int(lm.landmark[lmPose.LEFT_EAR].x * w)
+                    l_ear_y = int(lm.landmark[lmPose.LEFT_EAR].y * h)
+                    # Left hip.
+
+                    l_hip_x = int(lm.landmark[lmPose.LEFT_HIP].x * w)
+                    l_hip_y = int(lm.landmark[lmPose.LEFT_HIP].y * h)
+
+                    # Calculate angle.
+                    def findAngle(x1, y1, x2, y2):
+                        theta = m.acos((y2 -y1)*(-y1) / (m.sqrt((x2 - x1)**2 + (y2 - y1)**2) * y1))
+                        degree = int(180/m.pi)*theta
+                        return degree
                 
-                left_EyeSH = calculate_angle(left_eye,left_shoulder,left_hip)
-                right_EyeSH = calculate_angle(right_eye, right_shoulder, right_hip)
-                left_ESH = calculate_angle(left_ear,left_shoulder,left_hip)
-                right_ESH = calculate_angle(right_ear, right_shoulder, right_hip) 
-                left_NSH = calculate_angle(nose,left_shoulder,left_hip)
-                right_NSH = calculate_angle(nose, right_shoulder, right_hip)
-                left_SHK = calculate_angle(left_shoulder,left_hip, left_knee)
-                right_SHK = calculate_angle(right_shoulder, right_hip, right_knee)
-                
-                test = [left_EyeSH,right_EyeSH,left_ESH,right_ESH,left_NSH,right_NSH,left_SHK,right_SHK]
-                answer = clf.predict([test])
-                print(answer)
-                
-                print_ans="0"
-                
-                if answer==0:
-                    print_ans="Bad Posture"
-                    count = count+1
-                    # Beep sound
-                    if count >= 1 and count <= 5:
-                        duration = 0.5  # seconds
-                        frequency = 1000  # Hz
-                        t = np.linspace(0, duration, int(44100 * duration), endpoint=False)
-                        beep = 0.5 * np.sin(2 * np.pi * frequency * t)
-                        
-                        # Play the beep sound
-                        sd.play(beep, samplerate=44100)
-                        sd.wait()
-                else:
-                    print_ans = "Good Posture"
                     
-                draw.text((20,80), print_ans, font = font)
-                image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
-            except:
-                pass
+                    # setting image writeable back to true to be able process it
+                    image.flags.writeable = True
+                    pil_im = Image.fromarray(image)
+                    draw = ImageDraw.Draw(pil_im)
 
-            stframe.image(image,use_column_width=True)
+                    # def angle
+                    torso_angle = findAngle(l_shldr_x, l_shldr_y, l_hip_x, l_hip_y)
+                    torso_angle = "Shoulder-Hip Angle:" + str(round(torso_angle,2))
+                    neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
+                    neck_inclination = "Neck Inclination (Ear-Shoulder Angle):" + str(round(neck_inclination,2))
+                    
+                    # font
+                    font = ImageFont.truetype(font = "C:/Windows/Fonts/Arial.ttf", size = font_size)
+                    # font = ImageFont.truetype(font = "Arial Unicode.ttf", size = font_size)
+                    
+                    # draw text
+                    draw.text((20,0), torso_angle, font = font, fill = 'black')
+                   
+                    temp=0
+                    image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
+                    temp=temp+font_size+7
+                    draw.text((20,temp), neck_inclination, font = font, fill = 'black')
+                    temp=temp+temp
+                    image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
+
+                    # Draw the pose landmarks
+                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+                    
+                    len(results.pose_landmarks.landmark)
+                    num_coords = len(results.pose_landmarks.landmark)
+                    landmarks = ['class']
+                    for val in range(1, num_coords+1):
+                        landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+                    with open('Prediction_info.csv', mode='w', newline='') as f:
+                        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                        csv_writer.writerow(landmarks)
+                    class_name = 'abc'
+                except:
+                    pass
+                try:
+                    
+                    pose = results.pose_landmarks.landmark
+                    pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+                    row = pose_row 
+                    row.insert(0, class_name)
+                    
+                    with open('Prediction_info.csv', mode='a', newline='') as f:
+                        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                        csv_writer.writerow(row)
+
+                    df = pd.read_csv('Prediction_info.csv')
+                    nose = [df.at[0,'x1'],df.at[0,'y1']]
+                    
+                    left_eye = [df.at[0,'x3'],df.at[0,'y3']]
+                    left_ear = [df.at[0, 'x8'], df.at[0, 'y8']]
+                    left_shoulder = [df.at[0, 'x12'], df.at[0, 'y12']]
+                    left_hip = [df.at[0, 'x24'], df.at[0, 'y24']]
+                    left_knee = [df.at[0, 'x26'], df.at[0, 'y26']]
+
+                    right_eye = [df.at[0,'x6'],df.at[0,'y6']]
+                    right_ear = [df.at[0, 'x9'], df.at[0, 'y9']]
+                    right_shoulder = [df.at[0, 'x13'], df.at[0, 'y13']]
+                    right_hip = [df.at[0, 'x25'], df.at[0, 'y25']]
+                    right_knee = [df.at[0, 'x27'], df.at[0, 'y27']]
+                    
+                    left_EyeSH = calculate_angle(left_eye,left_shoulder,left_hip)
+                    right_EyeSH = calculate_angle(right_eye, right_shoulder, right_hip)
+                    left_ESH = calculate_angle(left_ear,left_shoulder,left_hip)
+                    right_ESH = calculate_angle(right_ear, right_shoulder, right_hip) 
+                    left_NSH = calculate_angle(nose,left_shoulder,left_hip)
+                    right_NSH = calculate_angle(nose, right_shoulder, right_hip)
+                    left_SHK = calculate_angle(left_shoulder,left_hip, left_knee)
+                    right_SHK = calculate_angle(right_shoulder, right_hip, right_knee)
+                    
+                    test = [left_EyeSH,right_EyeSH,left_ESH,right_ESH,left_NSH,right_NSH,left_SHK,right_SHK]
+                    answer = clf.predict([test])
+                    print(answer)
+                    
+                    print_ans="0"
+                    
+                    if answer==0:
+                        print_ans="Bad Posture"
+                        count = count+1
+
+                        # Beep sound
+                        if count >= 1 and count <= 5:
+                            duration = 0.5  # seconds
+                            frequency = 1000  # Hz
+                            t = np.linspace(0, duration, int(44100 * duration), endpoint=False)
+                            beep = 0.5 * np.sin(2 * np.pi * frequency * t)
+                            
+                            # Play the beep sound
+                            sd.play(beep, samplerate=44100)
+                            sd.wait()
+                    else:
+                        print_ans = "Good Posture"
+
+                    
+                    if int(font_size/10)<7:
+                        ss=font_size+27
+                    else:
+                        ss=int(font_size/10)+42+font_size
+                 
+                    font = ImageFont.truetype("C:/Windows/Fonts/Arial.ttf", ss)
+                    # font = ImageFont.truetype("Arial Unicode.ttf", ss)
+                    bbox = draw.textbbox((20,temp), print_ans, font = font)
+                    draw.rectangle(bbox, fill = 'black')
+                    draw.text((20,temp), print_ans, font = font, fill = 'white')
+                    
+                    image = cv2.cvtColor(np.array(pil_im), cv2.COLOR_RGB2BGR)
+                except:
+                    pass
+
+                stframe.image(image,use_column_width=True)
 
 
-        vid.release()
-        out.release()
-        cv2.destroyAllWindows()
+            vid.release()
+            out.release()
+            cv2.destroyAllWindows()
 
     st.success('Video is Processed')
     st.stop()
@@ -279,4 +316,4 @@ if __name__ == '__main__':
     main()
 
 
-# py -m streamlit run d:/capstone-running-form/test-beep.py
+# py -m streamlit run streamlit-app.py
